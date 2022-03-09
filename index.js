1d4d35b7751366ae439aa0c1f0257f1e3f34e6ce
Jake Eissler <jakeeissler1@gmail.com>
2022-03-08 22:39:28 -0700

    card generator is fully functional! Just have to add links for emails and github accounts.

--- index.js
+++ index.js
@@ -106,4 +106,47 @@ function managerInit() {
   });
 }
 managerInit();
-//function generateHTML() {} //TODO
+function generateHTML() {
+  console.log(`Cards being generated for all employees!`);
+
+  const html = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <title>Team Roster</title>
+  </head>
+  <body>
+    <div class="jumbotron text-center my-5">
+      <h1>Team Roster!</h1>
+    </div>
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+      ${employees
+        .map((employee) =>
+          employee.generateHTMLCard(
+            employee.officeNumber || employee.github || employee.school
+          )
+        )
+        .join("\n")}
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
+`;
+  fs.writeFileSync("./dist/output.html", html);
+  console.log(
+    "Check the 'output.html' file in the dist directory for your website!"
+  );
+}
